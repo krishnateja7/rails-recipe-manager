7015443b44752b5a09ca9576d50dcd65a252a187
krishnateja7 <krrishnateja7@gmail.com>
2016-06-21 12:52:16 -0400

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+
+  root 'pages#home'
+
+  get '/home', to: 'pages#home'
+
+  resources :recipes do
+    member do
+      post 'like'
+    end
+  end
+
+  resources :chefs, except: [:new]
+
+  get '/register', to: 'chefs#new'
+
+  get '/login', to: 'logins#new'
+  post '/login', to: 'logins#create'
+  get '/logout', to: 'logins#destroy'
+
+end
